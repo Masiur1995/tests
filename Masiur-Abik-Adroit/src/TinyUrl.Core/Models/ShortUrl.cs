2dcd86ece185b2f684915c0bfbe3b77d5494db5a
Masiur Abik <47774044+Masiur1995@users.noreply.github.com>
2026-01-14 16:38:20 -0500

    Add files via upload

---
+++ Masiur-Abik-Adroit/src/TinyUrl.Core/Models/ShortUrl.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TinyUrl.Core.Models;
+
+public class ShortUrl
+{
+    public string ShortCode { get; set; } = string.Empty;
+    public string LongUrl { get; set; } = string.Empty;
+    public int AccessCount { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
